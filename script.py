d3ff4f9856d6b820633446db62d59a1506a52ec1
Delphine <ddourdin@myges.fr>
2022-12-23 20:56:47 +0100

    Ajout du fichier de données

--- script.py
+++ script.py
@@ -1,12 +1,3 @@
 #!/usr/bin/env python
 
-import sys
-
-def factorielle(n):
-    if n == 0:
-        return 1
-    else:
-        return n  * factorielle(n-1)
-
-print(factorielle(int(sys.argv[1])))
-
+print("Hello World")
